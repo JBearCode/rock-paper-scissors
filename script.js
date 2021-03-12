5950214c69772ea4371495347afa93928e749838
JBearCode <joelbmoran@gmail.com>
2021-03-12 11:55:15 -0500

    Debug playRound, now works

--- script.js
+++ script.js
@@ -8,24 +8,27 @@ function computerPlay() {
     return availableChoices[randomChoice];
 }
 
+
+
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
 
     const winRound = "You won! " + playerSelection + " beats " + computerSelection;
-    const tieRound = "You tied." + playerSelection + " ties " + computerSelection;
-    const loseRound = "You lost!" + computerSelection + " beats " + playerSelection;
+    const tieRound = "You tied. " + playerSelection + " ties " + computerSelection;
+    const loseRound = "You lost! " + computerSelection + " beats " + playerSelection;
 
     if (playerSelection === computerSelection) {
         return tieRound;
-    } else if ((playerSelection = "rock" && computerSelection = "scissors") OR
-                (playerSelection = "paper" && computerSelection = "rock") OR
-                (playerSelection = "scissors" && computerSelection = "paper")) {
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return winRound;
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return winRound;
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
         return winRound;
     } else {
         return loseRound;
     }  
 }
 
-let playerSelection = "Rock";
+const playerSelection = "rock";
 const computerSelection = computerPlay();
-console.log(playRound(playerSelection));
+console.log(playRound(playerSelection, computerSelection));
