eb7c932ac5b9a0f3a1cf8a2d89edf0b6dfd7823a
JBearCode <joelbmoran@gmail.com>
2021-03-12 10:26:36 -0500

    Create array and use Math.random

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 "use strict";
 
+// 1. Make function choose 1 of 3 strings (or from array?)
+
+const availableChoices = ["rock", "paper", "scissors"];
+const randomChoice = Math.floor(Math.random() * availableChoices.length);
+return randomChoice;
+
 function computerPlay() {
-    
+
 }
 
 function playRound(playerSelection, computerSelection) {
