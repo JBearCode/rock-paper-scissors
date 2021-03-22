4413d9ffd7eb259e3bb0390013818881d7a1bf73
JBearCode <joelbmoran@gmail.com>
2021-03-22 12:04:04 -0400

    Create weighted function computerPlay()

--- scriptDOM.js
+++ scriptDOM.js
@@ -1,73 +1,32 @@
 "use strict";
 
-// weighted random outputs
+// play a random (weighted) choice
 function computerPlay() {
-    const availableChoices = ["rock", "paper", "scissors", "fire", "water"];
-    const choiceWeight = [1, 1, 1, 3, 3];
-    const totalWeight = eval.(choiceWeight.join("+"));
-    const currentChoice = 0;
+    let weightedChoices = [
+        ['rock', 1],
+        ['paper', 1],
+        ['scissors', 1],
+        ['fire', 3],
+        ['water', 3]
+    ];
 
-    while (currentChoice < availableChoices.length) {
-        for (i = 0; i < choiceWeight[currentChoice]; i++)
-        
+    // sum up weights from array above
+    let sumWeight = 0;
+    for (let i = 0; i < weightedChoices.length; i++) {
+        sumWeight += weightedChoices[i][1];
     }
 
-    const randomChoice = Math.floor(Math.random() * availableChoices.length);
-    return availableChoices[randomChoice];
-}
+    // select random number
+    const threshold = Math.random() * sumWeight;
 
-// playerPlay asks for choice then converts to lower case and returns answer
-function playerPlay() {
-    let playerAnswer = window.prompt("Choose rock, paper, or scissors.", "").toLowerCase();
-    return playerAnswer;
-}
+    // transfer number to array value
+    let counter = 0;
+    for (let i = 0; i < weightedChoices.length -1; ++i) {
+        counter += weightedChoices[i][1];
 
-// Keep score
-let playerScore = 0;
-let botScore = 0;
-
-// Function to capitalize first letter in a string (used in playRound function)
-function capitalize1stLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
-// playRound runs computerPlay() and playerPlay(), updates scores based on winner, and returns results
-function playRound(playerSelection, computerSelection) {
-    playerSelection = playerPlay();
-    computerSelection = computerPlay();
-
-    const winRound = "You won! " + capitalize1stLetter(playerSelection) + " beats " + computerSelection + ".";
-    const tieRound = "You tied. " + capitalize1stLetter(playerSelection) + " ties " + computerSelection + ".";
-    const loseRound = "You lost! " + capitalize1stLetter(playerSelection) + " loses to " + computerSelection + ".";
-    const currentScore = "\nYour score is " + playerScore + ". Computer score is " + botScore + ".";
-    const winGame = "\nYou won the game! Reload page to try again.";
-    const loseGame = "\nYou lose the game! Reload page to try again.";
-
-    if (playerSelection === computerSelection) {
-        return tieRound;
-    } else if ((playerSelection === "rock" && computerSelection === "scissors") 
-            || (playerSelection === "paper" && computerSelection === "rock")
-            || (playerSelection === "scissors" && computerSelection === "paper")) {
-        ++playerScore;
-        if (playerScore === 5) {
-            return winRound + currentScore + winGame;
+        if (counter >= threshold) {
+            return weightedChoices[i][0];
         }
-        else {
-            return winRound + currentScore;
-        }
-    } else {
-        ++botScore;
-        if (botScore === 5) {
-            return loseRound + currentScore + loseGame;
-        }
-        else {
-            return loseRound + currentScore;
-        }
-    }  
-}
-
-function game() {
-    while (playerScore < 5 && botScore < 5) {
-        console.log(playRound());
-    }
+    }   
+return weightedChoices[weightedChoices.length -1][0];
 }
