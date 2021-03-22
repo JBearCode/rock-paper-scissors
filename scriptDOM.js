94646d8e985fc6d86d964a50cc349d899bdfa498
JBearCode <joelbmoran@gmail.com>
2021-03-22 18:56:07 -0400

    Design HTML and CSS layout

--- scriptDOM.js
+++ scriptDOM.js
@@ -30,3 +30,49 @@ function computerPlay() {
     }   
 return weightedChoices[weightedChoices.length -1][0];
 }
+
+// Button constants
+const rock = document.getElementById('rockButton');
+const paper = document.getElementById('paperButton');
+const scissors = document.getElementById('scissorsButton');
+const fire = document.getElementById('fireButton');
+const water = document.getElementById('waterButton');
+
+rock.addEventListener('click', playRound(rock));
+paper.addEventListener('click', playRound(paper));
+scissors.addEventListener('click', playRound(scissors));
+fire.addEventListener('click', playRound(fire));
+water.addEventListener('click', playRound(water));
+
+function playRound (playerSelection, computerSelection) { 
+    computerSelection = computerPlay();
+
+const winRound = "You won! " + capitalize1stLetter(playerSelection) + " beats " + computerSelection + ".";
+const tieRound = "You tied. " + capitalize1stLetter(playerSelection) + " ties " + computerSelection + ".";
+const loseRound = "You lost! " + capitalize1stLetter(playerSelection) + " loses to " + computerSelection + ".";
+const currentScore = "\nYour score is " + playerScore + ". Computer score is " + botScore + ".";
+const winGame = "\nYou won the game! Reload page to try again.";
+const loseGame = "\nYou lose the game! Reload page to try again.";
+
+if (playerSelection === computerSelection) {
+    return tieRound;
+} else if ((playerSelection === "rock" && computerSelection === "scissors") 
+        || (playerSelection === "paper" && computerSelection === "rock")
+        || (playerSelection === "scissors" && computerSelection === "paper")) {
+    ++playerScore;
+    if (playerScore === 5) {
+        return winRound + currentScore + winGame;
+    }
+    else {
+        return winRound + currentScore;
+    }
+} else {
+    ++botScore;
+    if (botScore === 5) {
+        return loseRound + currentScore + loseGame;
+    }
+    else {
+        return loseRound + currentScore;
+    }
+}  
+}
