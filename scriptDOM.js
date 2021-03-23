8b04e03dbad57530e1fb264bb45b3660b9702cdd
JBearCode <joelbmoran@gmail.com>
2021-03-22 23:11:28 -0400

    Use onclick to launch main function

--- scriptDOM.js
+++ scriptDOM.js
@@ -1,47 +1,68 @@
 "use strict";
 
-// Keep score
 let playerScore = 0;
 let computerScore = 0;
+const mainPara = document.getElementById("mainPara");
 
-// Function to capitalize first letter in a string (used in playRound function)
-function capitalize1stLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+function playRound(buttonName) {
+    const playerSelection = buttonName;
+    const computerSelection = computerPlay();
+    updateScore(findWinner(playerSelection, computerSelection));
+    checkGameOver();
 }
 
-// Button constants
-const rockButton = document.getElementById('rockButton');
-const paperButton = document.getElementById('paperButton');
-const scissorsButton = document.getElementById('scissorsButton');
-const fireButton = document.getElementById('fireButton');
-const waterButton = document.getElementById('waterButton');
+function checkGameOver() {
+    if (playerScore < 5 && computerScore < 5) {
+        return;
+    } else {
+        endGame();
+    }
+}
 
-// initiate playRound function on click
-rockButton.addEventListener('click', function() { 
-    playRound("rock", computerPlay()) });
-paperButton.addEventListener('click', function() { 
-    playRound("paper", computerPlay()) });
-scissorsButton.addEventListener('click', function() { 
-    playRound("scissors", computerPlay()) });
-fireButton.addEventListener('click', function() { 
-    playRound("fire", computerPlay()) });
-waterButton.addEventListener('click', function() { 
-    playRound("water", computerPlay()) });
+function endGame() {
+    if (playerScore = 5) {
+    mainPara.textContent = "Woohoo! You won the game! 🥳"
+    } else {
+    mainPara.textContent = "Unfortunately, you've been bested by a mindless hunk of metal. 😬 "
+    }
+}
+
+
+
+    
+
+function updateScore(winner) {
+    const playerScorePara = document.getElementById("playerScorePara");
+    const computerScorePara = document.getElementById("computerScorePara");
+
+    if (winner === "player") {
+        ++playerScore;
+    } else if (winner === "computer") {
+        ++computerScore;
+    }
+
+    playerScorePara.textContent = `Player: ${playerScore}` ;
+    computerScorePara.textContent = `Computer: ${computerScore}`;
+}
 
 // function to return results of one round
 function findWinner (playerSelection, computerSelection) { 
 
-if (playerSelection === computerSelection) {
-    return "tie";
-} else if ((playerSelection === "rock" && computerSelection === "scissors") 
+    if (playerSelection === computerSelection) {
+        mainPara.textContent = "It's a tie! Lame."
+        return "tie";
+    } else if ((playerSelection === "rock" && computerSelection === "scissors") 
         || (playerSelection === "paper" && computerSelection === "rock")
         || (playerSelection === "scissors" && computerSelection === "paper")
         || (playerSelection === "water" && computerSelection === "fire" )
         || (playerSelection === "fire" && computerSelection !== "water" )) {
-    return "player"
-} else {
-    return "computer";
-} }
+            mainPara.textContent = "You win! " + capitalize1stLetter(playerSelection) + " beats " + computerSelection + "."
+            return "player";
+    } else {
+        mainPara.textContent = "You lose! " + capitalize1stLetter(playerSelection) + " loses to " + computerSelection + "."
+        return "computer";
+    }
+}
 
 // play a random (weighted) choice
 function computerPlay() {
@@ -73,4 +94,16 @@ function computerPlay() {
     }   
     return weightedChoices[weightedChoices.length -1][0];
 }
+
+// Function to capitalize first letter in a string (used in playRound function)
+function capitalize1stLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+// Button constants
+const rockButton = document.getElementById('rock');
+const paperButton = document.getElementById('paper');
+const scissorsButton = document.getElementById('scissors');
+const fireButton = document.getElementById('fire');
+const waterButton = document.getElementById('water');
 
